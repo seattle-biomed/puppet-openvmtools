5cbf43be70ebadd6041aaf764ce70a3dc118d5e0
Andrew Leonard <andy@hurricane-ridge.com>
2012-11-15 13:21:03 -0800

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+# == Class: openvmtools
+#
+# Puppet module to install and configure openvmtools on a VMware VM.
+#
+# === Parameters
+#
+# === Examples
+#
+#  include openvmtools
+#
+# === Authors
+#
+# Andrew Leonard
+#
+# === Copyright
+#
+# Copyright 2012 Andrew Leonard, Seattle Biomedical Research Institute
+#
+class openvmtools {
+
+
+}
